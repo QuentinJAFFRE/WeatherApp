4fc4499af6e869e9692a8a1679fce4c0eec36994
Quentin Jaffré <quentin.jaffre@epita.fr>
2022-12-30 16:46:45 +0100

    MAIN: init UI components

--- app/src/main/java/com/startup/weatherapp/MainActivity.kt
+++ app/src/main/java/com/startup/weatherapp/MainActivity.kt
@@ -2,8 +2,28 @@ package com.startup.weatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.RelativeLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+
+    private var homeRL: RelativeLayout = findViewById(R.id.idRLHome);
+    private var loadingPB: ProgressBar = findViewById(R.id.idPBLoading);
+    private var cityNameTV: TextView = findViewById(R.id.idTVCityName);
+    private var temperatureTV: TextView = findViewById(R.id.idTVTemperature);
+    private var conditionTV: TextView? = findViewById(R.id.idTVCondition);
+    private var cityEdt: TextInputEditText? = findViewById(R.id.idEdtCity);
+    private var backIV: ImageView? = findViewById(R.id.idIVBack);
+    private var iconIV: ImageView? = findViewById(R.id.idIVIcon);
+    private var searchIV: ImageView? = findViewById(R.id.idIVSearch);
+    private var weatherRV: RecyclerView? = findViewById(R.id.idRVWeather);
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
