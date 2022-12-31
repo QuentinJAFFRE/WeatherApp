041858e49d9786555b4219ec3714f0761f0c8616
Quentin Jaffré <quentin.jaffre@epita.fr>
2022-12-31 10:38:42 +0100

    ARCHI: dev adapter to send models to UI

--- app/src/main/java/com/startup/weatherapp/MainActivity.kt
+++ app/src/main/java/com/startup/weatherapp/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.startup.weatherapp
 
+import android.content.Context
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.RelativeLayout
@@ -11,21 +14,53 @@ import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
 
-    private var homeRL: RelativeLayout = findViewById(R.id.idRLHome);
-    private var loadingPB: ProgressBar = findViewById(R.id.idPBLoading);
-    private var cityNameTV: TextView = findViewById(R.id.idTVCityName);
-    private var temperatureTV: TextView = findViewById(R.id.idTVTemperature);
-    private var conditionTV: TextView? = findViewById(R.id.idTVCondition);
-    private var cityEdt: TextInputEditText? = findViewById(R.id.idEdtCity);
-    private var backIV: ImageView? = findViewById(R.id.idIVBack);
-    private var iconIV: ImageView? = findViewById(R.id.idIVIcon);
-    private var searchIV: ImageView? = findViewById(R.id.idIVSearch);
-    private var weatherRV: RecyclerView? = findViewById(R.id.idRVWeather);
+    private var homeRL: RelativeLayout? = null
+    private var loadingPB: ProgressBar? = null
+    private var cityNameTV: TextView? = null
+    private var temperatureTV: TextView? = null
+    private var conditionTV: TextView? = null
+    private var cityEdt: TextInputEditText? = null
+    private var backIV: ImageView? = null
+    private var iconIV: ImageView? = null
+    private var searchIV: ImageView? = null
+    private var weatherRV: RecyclerView? = null
 
+    private var weatherRVModalArrayList: ArrayList<WeatherRVModal>? = null;
+    private var weatherRVAdapter: WeatherRVAdapter? = null
 
+    private var locationManager: LocationManager? = null;
+    private var PERMISSION_CODE: Int =  1;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Hide the status bar.
+        getWindow().setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
+
         setContentView(R.layout.activity_main)
+        homeRL = findViewById(R.id.idRLHome);
+        loadingPB = findViewById(R.id.idPBLoading);
+        cityNameTV = findViewById(R.id.idTVCityName);
+        temperatureTV = findViewById(R.id.idTVTemperature);
+        conditionTV = findViewById(R.id.idTVCondition);
+        cityEdt = findViewById(R.id.idEdtCity);
+        backIV = findViewById(R.id.idIVBack);
+        iconIV = findViewById(R.id.idIVIcon);
+        searchIV = findViewById(R.id.idIVSearch);
+        weatherRV = findViewById(R.id.idRVWeather);
+        
+        weatherRVModalArrayList = ArrayList<WeatherRVModal>();
+        weatherRVAdapter = WeatherRVAdapter(this, weatherRVModalArrayList!!);
+
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager?;
+
+    }
+
+    private fun getWeatherInfo(cityName: String) {
+        val API_KEY: String = "41bb9c94c9c5435fbee150942223012"
+        val url: String = "https://api.weatherapi.com/v1/forecast.json?key=" + API_KEY + "&q=" + cityName + "&days=3&aqi=no&alerts=no";
     }
 }
--- app/src/main/java/com/startup/weatherapp/WeatherRVAdapter.kt
+++ app/src/main/java/com/startup/weatherapp/WeatherRVAdapter.kt
@@ -1,4 +1,57 @@
 package com.startup.weatherapp
 
-class WeatherRVAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
+
+class WeatherRVAdapter(
+    private val context: Context,
+    private val weatherRVModalArrayList: ArrayList<WeatherRVModal>) :
+    RecyclerView.Adapter<WeatherRVAdapter.ViewHolder>() {
+
+
+    @Override
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view: View = LayoutInflater.from(context).inflate(R.layout.weather_rv_item, parent, false);
+        return ViewHolder(view);
+    }
+
+    @Override
+    override fun onBindViewHolder(holder: WeatherRVAdapter.ViewHolder, position: Int) {
+        val modal: WeatherRVModal = weatherRVModalArrayList[position];
+        holder.tempereratureTV.text = modal.temperature + "°C";
+        holder.windTV.text = modal.windSpeed + "km/h";
+        val iconURL = "http:" + modal.icon;
+        Picasso.get().load(iconURL).into(holder.conditionIV);
+        val input: SimpleDateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm");
+        val output: SimpleDateFormat = SimpleDateFormat("HH:mm aa");
+        try {
+            val date: Date = input.parse(modal.time);
+            val timeToDisplay = output.format(date);
+            holder.timeTV.text = timeToDisplay;
+        } catch (e: Exception) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    override fun getItemCount(): Int {
+        return weatherRVModalArrayList.size;
+    }
+
+    class ViewHolder(private val itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val windTV: TextView = itemView.findViewById(R.id.idTVWindSpeed);
+        val tempereratureTV: TextView = itemView.findViewById(R.id.idTVTemperature);
+        val timeTV: TextView = itemView.findViewById(R.id.idTVTime);
+        val conditionIV: ImageView = itemView.findViewById(R.id.idIVCondition);
+
+    }
 }
